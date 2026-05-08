b9b0b8e6e0fd565c65be8aaceceec9ec45ede716
obabakir <bubufighting01@gmail.com>
2026-05-08 18:19:26 +0900

    feat: learn numbers in python

---
+++ primative.py
@@ -0,0 +1,11 @@
+print("==========")
+# in Java: variable is a name of storage location
+# in Python: variable is named referance
+
+count = 100
+type_count = type(count)
+print(f"the count: {count} and the type of count: {type_count}")
+
+result1 = count.bit_count()
+result2 = count.numerator
+print(result1, result2)
