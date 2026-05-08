e854ae1a0d39613cc0f55e85f0db349e07a8ce82
obabakir <bubufighting01@gmail.com>
2026-05-08 19:54:14 +0900

    feat: learning boolean in Python

--- primative.py
+++ primative.py
@@ -39,3 +39,37 @@ print(course)
 # print(f"the result (4): {result}")
 # # => biz course ga tenglangan result qiymatni ozgartirayapmis shunga  asl coursre.value ozgarmadi
 # print(f"new_course.value: {course}")
+
+print("===  booolean  ===")
+# Functions:
+'''
+type() => finds type of object value
+input() => terminalga kiritilgan valueni qiymatini bertadi
+bool() => Truthy = true, 100, -100, "MIT" 
+       => Falsy = false, 0, " ",  
+       => None
+str() => changes number to string
+int() => changes string to number
+'''
+
+y = input("give your value for y:")
+print(f"y: {y}")
+# >>>>   give your value for y:30
+# >>>>   y: 30
+
+# test_falsy = ""
+# Note: in javascript + node.js da operatorlardan :
+# && → AND
+# || → OR
+# ?? → Nullish coalescing
+# ?. → Optional chaining
+# Pythonda bu < or >
+test_falsy = "" or 0 or 100
+# true qatdi chunki 1 ta 100 holatda true ligi uchun
+print("test_falsy", bool(test_falsy))
+
+# test_truthy = "MIT group"
+test_truthy = "MIT group" and 100
+# and 0 ni qoshsak False olamiz
+# true qatdi chunki 1 ta 100 holatda true ligi uchun
+print("test_truthy", bool(test_truthy))
